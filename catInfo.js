38e8573423b6f1dfb04c28213dcf86cce9c6ba53
Aala <ayla99n@gmail.com>
2026-02-14 17:10:33 -0500

    Initial commit - Catdex

---
+++ catInfo.js
@@ -0,0 +1,91 @@
+const API_KEY = import.meta.env.VITE_CAT_API_KEY;
+
+//const API_KEY = "live_emuDArEHr3IIBc79w6iA5osDOTz9N6LGwCmLjZ9ZrInTAboFqo5OqHz8m2CPunOs"
+
+let breeds = [];
+
+// function homepage() {
+//   window.location.href = "index.html";
+// }
+
+const params = new URLSearchParams(window.location.search);
+const breedId = params.get("id");
+
+async function fetchBreeds() {
+  const response = await fetch("https://api.thecatapi.com/v1/breeds", {
+    headers: { "x-api-key": API_KEY }
+  });
+
+  breeds = await response.json();
+
+  breedDropdown();
+  fetchBreedInfo(breedId);
+}
+
+function breedDropdown() {
+    const datalist = document.getElementById("breedList");
+    datalist.innerHTML = "";
+
+    breeds.forEach(breed => {
+    const option = document.createElement("option");
+    option.value = breed.name; //filter breeds
+    datalist.appendChild(option);
+
+  });
+}
+
+async function fetchBreedInfo(breedId) {
+  if (!breedId) return;
+
+  const breed = breeds.find(b => b.id === breedId);
+
+  if (!breed) return;
+
+  // Fetch image
+  const imageResponse = await fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breed.id}`,
+    { headers: { "x-api-key": API_KEY } }
+  );
+  const imageData = await imageResponse.json();
+  const imageUrl = imageData[0]?.url;
+
+  displayBreedInfo(breed, imageUrl);
+}
+
+//how to bold and underline 
+function displayBreedInfo(breed, imageUrl) {
+  document.getElementById("catName").textContent = breed.name;
+
+  document.getElementById("catDescription").innerHTML =
+    `<strong>Description:</strong> ${breed.description}`;
+
+  document.getElementById("catOrigin").innerHTML =
+    `<strong>Origin:</strong> ${breed.origin}`;
+
+  document.getElementById("catTemperament").innerHTML =
+    `<strong>Temperament:</strong> ${breed.temperament}`;
+
+  document.getElementById("catLifeSpan").innerHTML =
+    `<strong>Life Span:</strong> ${breed.life_span} years`;
+
+  document.getElementById("wikiUrl").innerHTML =
+    `<strong>Wikipedia:</strong> <a href="${breed.wikipedia_url}" target="_blank">${breed.name} cat Wiki page</a>`;
+
+  document.getElementById("catImage").src = imageUrl;
+}
+
+
+fetchBreeds();
+document.getElementById("searchButton").addEventListener("click", () => {
+  const input = document.getElementById("breedInput").value.toLowerCase();
+
+  const foundBreed = breeds.find(
+    b => b.name.toLowerCase() === input
+  );
+
+  if (foundBreed) {
+    window.location.href = `/catInfo.html?id=${foundBreed.id}`;
+  } else {
+    alert("Breed not found");
+  }
+});
---
+++ homepage.js
@@ -0,0 +1,116 @@
+const API_KEY = import.meta.env.VITE_CAT_API_KEY;
+
+//const API_KEY = "live_emuDArEHr3IIBc79w6iA5osDOTz9N6LGwCmLjZ9ZrInTAboFqo5OqHz8m2CPunOs"
+console.log("API key:", import.meta.env.VITE_CAT_API_KEY);
+
+
+let breeds = [];
+
+//fetch breeds 
+async function fetchBreeds() {
+    const response = await fetch("https://api.thecatapi.com/v1/breeds", {
+    headers: { "x-api-key": API_KEY }
+  });
+
+    breeds = await response.json();
+    breedDropdown();
+    randomDex(8);
+}
+
+function breedDropdown() {
+    const datalist = document.getElementById("breedList");
+    datalist.innerHTML = "";
+
+    breeds.forEach(breed => {
+    const option = document.createElement("option");
+    option.value = breed.name; //filter breeds
+    datalist.appendChild(option);
+
+  });
+}
+
+//fetch breed image
+async function fetchBreedImage(breedId) {
+  const response = await fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`,
+    { headers: { "x-api-key": API_KEY } }
+  );
+
+  const data = await response.json();
+  return data[0]?.url;
+}
+
+
+//display breeds
+async function randomDex(count) {
+  const container = document.getElementById("dexContainer");
+  container.innerHTML = ""; // clear old cards
+
+  for (let i = 0; i < count; i++) {
+    const randomBreed =
+      breeds[Math.floor(Math.random() * breeds.length)];
+
+    const imageUrl = await fetchBreedImage(randomBreed.id);
+
+    createCard(randomBreed, imageUrl);
+  }
+}
+
+
+//create card
+function createCard(breed, imageUrl) {
+  const container = document.getElementById("dexContainer");
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <img src="${imageUrl}" alt="${breed.name}">
+    <h2>${breed.name}</h2>
+    <p>${breed.temperament}</p>
+  `;
+
+ 
+  card.addEventListener("click", () => {
+  window.location.href = `catInfo.html?id=${breed.id}`;
+  });
+
+
+  container.appendChild(card);
+}
+
+
+
+async function searchBreed() {
+  const searchValue = document
+    .getElementById("breedInput")
+    .value
+    .toLowerCase();
+
+  const foundBreed = breeds.find(breed =>
+    breed.name.toLowerCase().includes(searchValue)
+  );
+
+  if (foundBreed) {
+    window.location.href = `catInfo.html?id=${foundBreed.id}`;
+  } else {
+    alert("No breed found with that name");
+  }
+}
+
+document.getElementById("randomBtn").addEventListener("click", () => {
+  randomDex(8);
+});
+
+document.getElementById("searchButton").addEventListener("click", () => {
+  const input = document.getElementById("breedInput").value.toLowerCase();
+  const foundBreed = breeds.find(b => b.name.toLowerCase() === input);
+  if (foundBreed) {
+    window.location.href = `catInfo.html?id=${foundBreed.id}`;
+  } else {
+    alert("Breed not found");
+  }
+});
+
+// Load on page start
+fetchBreeds();
